fba0c78a43defc67d296df4ab5c95cb5b2a9c786
Joe Adams <joe.adams@cosmitaldesigns.com>
2016-10-25 17:59:46 -0400

    Test commit to Online Repo.

---
+++ TestProjectForBitBucket/TestProjectForBitBucket/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TestProjectForBitBucket.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ TestProjectForBitBucket/TestProjectForBitBucket/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestProjectForBitBucket.WebApiApplication" Language="C#" %>
